10993fcc6e281cc0abadc15e99d85210fda5a8af
TristanKatte <tristankatte@hotmail.com>
2025-07-18 00:49:54 +0200

    finishing up

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import type { LayoutLoad } from './$types';
 import type { Post } from '$lib/types';
 
+export const prerender = true;
+
 export const load: LayoutLoad = async ({ fetch }) => {
 	const res = await fetch('/api/posts');
 	const posts: Post[] = await res.json();
